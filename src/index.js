9bb8aa0f16f449bdb4889a78890c565dde573fdd
gunhwalee <ghlee2588@gmail.com>
2023-07-13 19:28:13 +0900

    build babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const helloDiv = document.createElement("div");
-helloDiv.innerHTML = "Hello JavaScript and webpack!";
+const sayHello = (times) =>
+  new Array(times).fill(1).map((_, i) => `Hello ${i + 1}`);
+
+helloDiv.innerHTML = sayHello(10).join("<br/>");
 document.body.appendChild(helloDiv);
