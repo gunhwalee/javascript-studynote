520802e5f92b9e27a5f56237d160c9ca74bda25c
gunhwalee <ghlee2588@gmail.com>
2023-07-13 18:05:05 +0900

    add webpack dev server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 const helloDiv = document.createElement("div");
-helloDiv.innerHTML = "Hello JavaScript";
+helloDiv.innerHTML = "Hello JavaScript and webpack!";
 document.body.appendChild(helloDiv);
--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,10 @@ module.exports = {
       template: path.resolve(__dirname, "public", "index.html"),
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "build"),
+    },
+    port: 3000,
+  },
 };
